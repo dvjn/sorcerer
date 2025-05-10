ed92eb9c70db90130ffee74ff61abf006bc7d0c3
Divya Jain <git@dvjn.dev>
2025-05-11 01:09:46 +0530

    fix api struct

--- internal/api/api.go
+++ internal/api/api.go
@@ -9,21 +9,21 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-type API struct {
+type Api struct {
 	distribution *distribution.Distribution
 }
 
-func New(distribution *distribution.Distribution) *API {
-	return &API{distribution: distribution}
+func New(distribution *distribution.Distribution) *Api {
+	return &Api{distribution: distribution}
 }
 
-func (a *API) Router() *chi.Mux {
+func (a *Api) Router() *chi.Mux {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
-	r.Get("/", index)
-	r.Get("/healthz", heartbeat)
+	r.Get("/", a.index)
+	r.Get("/healthz", a.heartbeat)
 
 	r.Mount("/v2", a.distribution.Router())
 
@@ -33,12 +33,12 @@ func (a *API) Router() *chi.Mux {
 //go:embed banner.txt
 var sorcererBanner []byte
 
-func index(w http.ResponseWriter, r *http.Request) {
+func (a *Api) index(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(sorcererBanner))
 	w.WriteHeader(http.StatusOK)
 }
 
-func heartbeat(w http.ResponseWriter, r *http.Request) {
+func (a *Api) heartbeat(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("."))
 	w.WriteHeader(http.StatusOK)
 }
